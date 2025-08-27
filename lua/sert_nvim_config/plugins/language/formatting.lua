13fcfef8d7459de80816718260bf49f67b7c3b82
Raspirat <malte.rauschenbach@tum.de>
2025-08-27 20:08:43 +0200

    Formatting stuff

--- lua/sert_nvim_config/plugins/language/formatting.lua
+++ lua/sert_nvim_config/plugins/language/formatting.lua
@@ -26,11 +26,16 @@ return {
         bib = { "bibtex-tidy" },
         cargo = { "clippy" },
       },
-      format_on_save = {
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 1000,
-      },
+      format_on_save = function(bufnr)
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+        return {
+          lsp_fallback = true,
+          async = false,
+          timeout_ms = 1000,
+        }
+      end
     })
 
     vim.keymap.set({ "n", "v" }, "<leader>lf", function()
@@ -40,5 +45,16 @@ return {
         timeout_ms = 1000,
       })
     end, { desc = "Format file or range (in visual mode)" })
+
+    vim.api.nvim_create_user_command("ToggleFormat", function(args)
+      if args.bang then
+        vim.b.disable_autoformat = not vim.b.disable_autoformat
+      else
+        vim.g.disable_autoformat = not vim.g.disable_autoformat
+      end
+    end, {
+      desc = "toggle autoformat-on-save",
+      bang = true,
+    })
   end,
 }
